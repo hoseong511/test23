9dea3d933693e9b4535bda444932866780bf7b08
hoseong511 <thdghtjd115@naver.com>
2021-03-03 11:16:24 +0900

    update App.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # movieApp
+
+### 1. Set up
+
+### 2. JSX & Props
+- React에서만 사용되는 jsx를 추가로 알고있으면 된다. 
--- moviehoho/src/App.js
+++ moviehoho/src/App.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React from "react"; //jsx가 인식하기 위한 장치
+
+function Potato({ fav }){
+  return ( <h3>I love {fav}</h3>)
+}
 
 function App() {
   return (
@@ -6,6 +10,11 @@ function App() {
             <h1>
               Hello!!!
             </h1>
+            <Potato fav="hoho"/>
+            <Potato fav="juice"/>
+            <Potato fav="book"/>
+            <Potato fav="music"/>
+
           </div>
         );
      
