99bf68679e09fce096b8b079f0b6555ba2985b50
hoseong511 <thdghtjd115@naver.com>
2021-03-07 22:48:54 +0900

    create index.js module

--- moviehoho/src/index.js
+++ moviehoho/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Hoho from './Head';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('App')
+);
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Hoho />
+  </React.StrictMode>,
+  document.getElementById('Head')
 );
 
 
