e599f497dd274a01f2c7576d8c9f4644d8190220
hoseong511 <thdghtjd115@naver.com>
2021-03-07 19:53:32 +0900

    update

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # 노마드코더와 함께하는 리액트 movieApp
 
-### 1. Set up
+## 1. Set up
 
-### 2. JSX & Props
+## 2. JSX & PROPS
+
+### 2.1 Reusable Components with JSX + Props
 - React에서만 사용되는 jsx를 추가로 알고있으면 된다. 
 - index.html 에서 ```<div id="App">```에 뿌려지게 된다.
 <p align="center"><img width="500" src="https://user-images.githubusercontent.com/62678380/110234770-bafa5f00-7f6f-11eb-8ab4-4e9f94af8e4f.png"> </p>
 
-### 3. Dynamic Component Generation
-- 오늘의 핵심은 JavaScript 문법 중 map을 이용해서 array안에 내용을 자동으로 뿌려주자.
+### 2.2 Dynamic Component Generation
+- 핵심은 JavaScript 문법 중 map을 이용해서 array안에 내용을 반복적으로 뿌려주자.
     ```JavaScript
     friends = ["a", "b", "c", "d"]
     friends.map(function(current) => {
@@ -17,4 +19,12 @@
         return current
     })
     ```
-    
+### 2.3 map Recap
+- 오류 해결하기!
+<p align="center"><img src="https://user-images.githubusercontent.com/62678380/110237182-b89f0180-7f7d-11eb-9018-48af1f36a142.png" width="500"></p>
+
+```Html
+<ColorList key={color.id} name={color.name} image={color.image} />
+
+``` 
+
--- moviehoho/src/App.js
+++ moviehoho/src/App.js
@@ -1,35 +1,32 @@
 import React from "react"; //jsx가 인식하기 위한 장치
 
-function ColorList({name, image }){
-  return ( <div align="center">
-              <h2>It's {name}</h2>
-              <img src={image} width="300"/>
-          </div>
-          )
-}
-
 const myColor =[
   {
+    id:1,
     name: "Red",
     image: 
         "https://images.pexels.com/photos/1789970/pexels-photo-1789970.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
   },
   {
+    id:2,
     name: "Yellow",
     image: 
         "https://images.pexels.com/photos/5325845/pexels-photo-5325845.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
   },
   {
+    id:3,
     name: "Green",
     image: 
         "https://images.pexels.com/photos/1125328/pexels-photo-1125328.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
   },
   {
+    id:4,
     name: "Blue",
     image: 
         "https://images.pexels.com/photos/3530402/pexels-photo-3530402.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
   },
   {
+    id:5,
     name: "Purple",
     image: 
         "https://images.pexels.com/photos/2524952/pexels-photo-2524952.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -37,13 +34,26 @@ const myColor =[
 
 ]
 
+
+function ColorList({name, image }){
+  return ( <div align="center">
+              <h2>It's {name}</h2>
+              <img alt={name} src={image} width="300"/>
+          </div>
+          )
+}
+// function renderColor(color){
+//   console.log(color.name, color.image);
+//   return <ColorList name={color.name} image={color.image} />
+// }
+
 function App() {
   return (
           <div>
-            {myColor.map(color => (
-              <ColorList name={color.name} image={color.image} />
-            ))}
-
+            {myColor.map(color =>(
+            <ColorList key={color.id} name={color.name} image={color.image} />
+            ))
+            }
           </div>
         );
      
