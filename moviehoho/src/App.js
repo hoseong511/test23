e793e8858aadb820dda78a0925316fb0802c20c3
hoseong511 <thdghtjd115@naver.com>
2021-03-08 20:05:20 +0900

    update App.js Planning the Movie component

--- moviehoho/src/App.js
+++ moviehoho/src/App.js
@@ -2,40 +2,21 @@ import React from "react"; //jsx가 인식하기 위한 장치
 import PropTypes from "prop-types"; 
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count +1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count -1 }));
+    isLoading: true,
+    movies: []
   };
   componentDidMount(){
-    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-  componentWillUnmount(){
-    console.log("GoodBye ")
-  }
-  render(){
-    console.log("render")
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-        
-        
-      </div>
-    )
-  }
+  render(){       
+      const { isLoading } = this.state;
+      return <div>
+                {isLoading ? "Loading...": "We are ready"}                        
+             </div>;
+    }
 }
 
 export default App;
