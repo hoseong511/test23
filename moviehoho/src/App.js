c0abab728b6f7ec73f36b25bb8f7f1aa36042a4d
hoseong511 <thdghtjd115@naver.com>
2021-03-10 07:03:03 +0900

    update App.js axios

--- moviehoho/src/App.js
+++ moviehoho/src/App.js
@@ -1,4 +1,5 @@
 import React from "react"; //jsx가 인식하기 위한 장치
+import axios from "axios";
 import PropTypes from "prop-types"; 
 
 class App extends React.Component{
@@ -6,13 +7,21 @@ class App extends React.Component{
     isLoading: true,
     movies: []
   };
+  getMovies = async() => {
+    try {
+      const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+      console.log(movies);
+    } catch (error) {
+      console.error(error);
+    } 
+
+  }
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render(){       
-      const { isLoading } = this.state;
+      const { isLoading } = this.state;     
+      
       return <div>
                 {isLoading ? "Loading...": "We are ready"}                        
              </div>;
