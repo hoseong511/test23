bf24eb22bb90c3fa6e96a8ff78b6ba2f3da1561d
hoseong511 <thdghtjd115@naver.com>
2021-03-10 19:21:46 +0900

    update App.js Movie.js css

--- moviehoho/src/App.js
+++ moviehoho/src/App.js
@@ -1,7 +1,7 @@
 import React from "react"; //jsx가 인식하기 위한 장치
 import axios from "axios";
 import Movie from "./Movie"
-import PropTypes from "prop-types";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -10,8 +10,7 @@ class App extends React.Component {
   };
   getMovies = async () => {
     try {
-      const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
-      console.log(movies);
+      const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")      
       this.setState({ movies: movies, isLoading: false })
 
     } catch (error) {
@@ -25,11 +24,32 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
 
-    return <div>
-      {isLoading ? "Loading..." : movies.map(movie => {
-        console.log(movie);
-        return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
-      })}</div>;
+    return (
+      <section className="container">
+          {isLoading 
+          ? (<div className="loader">
+            <span className="lader__text">Loading...</span>
+          </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie => (                            
+                <Movie 
+                  key={movie.id} 
+                  id={movie.id}
+                  year={movie.year} 
+                  title={movie.title}  
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />                
+              ))}
+            </div>
+             )
+          }           
+      </section>
+      
+    )
+      
   }
 }
 
