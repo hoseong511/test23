8c9cea12df92f9dff0451ad3d7949e2d3c95935c
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-23 13:33:17 +0900

    update gh pages & static version

--- src/routes/About.js
+++ src/routes/About.js
@@ -7,9 +7,11 @@ function About(props){
         <div className="about__container">
             <span >About this page: I built it because to practice React!!</span>
             <br />
-            <a href="https://github.com/hoseong511">hoseong511</a>
-            <br></br>
-            <a href="https://github.com/hoseong511/moviehoho">DOC</a>
+            <div className="about__me">
+                <a href="https://github.com/hoseong511">hoseong511</a>
+                <br></br>
+                <a href="https://github.com/hoseong511/moviehoho">DOC</a>
+            </div>
         </div>
     )
 }
